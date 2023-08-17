618f7288521d77172a8e80974be079bf81d97124
sathwika tammala <121660731+Sathwika08@users.noreply.github.com>
2023-08-17 17:00:56 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,50 @@
+#While loop
+num=1
+while num<=5:
+    print(num)
+    num+=1
+    
+num=1
+sum_of_numbers = 0 
+while num <= 10:
+    sum_of_numbers += num
+    num += 1
+    print("sum of numbers from 1 to 10:", sum_of_numbers)  
+    
+    
+    
+    
+num=1
+while num<=10:
+    print(num)
+    num +=1
+    
+num = 1
+while num <=5 :
+    sum_of_numbers += 1
+    num += 1
+    
+print (f"The sum of numbers is :" ,sum_of_numbers)
+
+
+num=2
+while num<=10:
+    print(num)
+    num+=2
+    
+num=10
+while num>=1:
+    num -=1
+    print(num)
+    
+secret_num=9  
+while True:
+    guess=int(input("guess the number:"))
+    if guess == secret_num:
+        print("you guessed the number")
+    else:
+        print("you have entered the incorrect number")
+Break
+    
+    
+  
