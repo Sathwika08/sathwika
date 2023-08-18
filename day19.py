e60d3e4fc6a8e9286f02624d222d9892e3ba907b
sathwika tammala <121660731+Sathwika08@users.noreply.github.com>
2023-08-18 18:50:59 +0530

    Add files via upload

---
+++ day19.py
@@ -0,0 +1,31 @@
+#dictionaries
+student = {
+    "name": "sathwika",
+    "age": 20,
+    "major": "Computer Science",
+    "gpa": 9.0
+}
+
+print(student["name"])  
+print(student["gpa"])
+
+empty_dict={}
+print(empty_dict)
+
+ages_dict={"sathwika":20,"abhi":20}
+print(ages_dict)
+
+person_dict = {"name": "Alice", "city": "New York", "age": 30}
+city_value = person_dict["city"]
+print(city_value)
+
+contact_dict = {"name": "Bob", "email": "bob@example.com"}
+print(contact_dict)  
+
+info_dict = {"name": "John", "age": 22, "score": 85}
+score_value = info_dict["score"]
+print(score_value)  
+
+rectangle_dict = {"width": 10, "height": 5}
+print(rectangle_dict)  
+
