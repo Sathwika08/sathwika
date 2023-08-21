478b83118a470998a7d26d962006f39993e87247
sathwika tammala <121660731+Sathwika08@users.noreply.github.com>
2023-08-21 21:50:49 +0530

    Add files via upload

---
+++ day23.py
@@ -0,0 +1,16 @@
+#functions
+def greet():
+    print("hello world!")
+greet()
+
+def greet_user(name):
+    print(f"Hello, {name}!")
+greet_user("Alice")
+
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(3, 5)
+print(result)  
+
+
+  
